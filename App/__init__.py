816b08df7884a46d9b927289f08f6c7e1950e7a8
H Sravan Reddy <sravankr96@gmail.com>
2015-10-01 23:41:53 +0530

    First Commit

--- App/__init__.py
+++ App/__init__.py
@@ -6,3 +6,16 @@ Using pyqt5 for the gui og the application
 
 import sys
 from pyqt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250,150)
+    w.move(300,300)
+    w.setWindowTitle('Sample')
+    w.show()
+
+    sys.exit(app.exec_())
